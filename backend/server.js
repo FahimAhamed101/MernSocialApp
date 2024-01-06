a4278ba47539797a48719a6b9b68c21ef939bdef
fahim <fahimahamedweb@gmail.com>
2024-01-06 22:54:32 +0600

    first commit

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose")
+const dotenv = require("dotenv");
+const cors = require("cors");
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGODB).then(()=>
+console.log("DB connection successfull")).catch(()=>{
+          console.log("Some error occured")
+})
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(5000 , ()=>{
+          console.log("Server is running")
+})
